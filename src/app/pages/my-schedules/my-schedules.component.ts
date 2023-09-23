1947d7bfe100c8922d86d386bad819e20ac4f42e
leandrosilvacampos <1411471@sga.pucminas.br>
2023-09-23 09:49:58 -0300

    feat: :construction: Componente de criar novo agendamento

--- src/app/pages/my-schedules/my-schedules.component.ts
+++ src/app/pages/my-schedules/my-schedules.component.ts
@@ -2,24 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { IScheduling } from 'src/app/core/interfaces/scheduling';
 import { SchedulesHttpService } from 'src/app/services/schedules.service';
 
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+const ELEMENT_DATA = [
+  {
+    status: 'Agendado',
+    date: '23/09/2023 08:00 - 09:00',
+    company: 'Center clinica',
+    procedure: 'Consulta',
+  },
 ];
 
 @Component({
@@ -28,7 +17,7 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./my-schedules.component.scss'],
 })
 export class MySchedulesComponent implements OnInit {
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
+  displayedColumns: string[] = ['status', 'date', 'company', 'procedure'];
   dataSource = ELEMENT_DATA;
   userSchedules: IScheduling[] = [];
 
